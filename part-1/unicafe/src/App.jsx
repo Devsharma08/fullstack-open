a632b1387fbb5285d823fb5a9879e55ed9017d05
DevSharma08 <devsharma08072004@gmail.com>
2026-08-18 11:53:24 +0530

    feat:add solution for problem 1.11

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -26,12 +26,34 @@ const Statistics = (props) => {
     {
       (all === 0) ? <p>No feedback given</p> : (
       <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive + ' %'} />
+        <table>
+          <tbody>
+            <tr>
+              <td>good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>all</td>
+              <td>{all}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{average}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{positive.toFixed(1)} %</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
      )
   } 
