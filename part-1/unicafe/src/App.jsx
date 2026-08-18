c1a9f0faf191b92112c9ea8a78617fc60ea369c7
DevSharma08 <devsharma08072004@gmail.com>
2026-08-18 11:26:22 +0530

    feat:add 1.6 prob

---
+++ part-1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+const App = () => {
+  const [good,setGood] = useState(0);
+  const [neutral,setNeutral] = useState(0);
+  const [bad,setBad] = useState(0); 
+
+  return (
+    <div>
+      <div>
+        <h2>Give feedback</h2>
+        <div >
+              <button onClick={() => setGood(good + 1)}>
+                good
+              </button>
+              <button onClick={() => setNeutral(neutral + 1)}>
+                neutral
+              </button>
+              <button onClick={() => setBad(bad + 1)}>
+                bad
+              </button>
+        </div>
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+        </div>
+      </div>
+    </div>
+  )
+ }
+
+
+export default App;
